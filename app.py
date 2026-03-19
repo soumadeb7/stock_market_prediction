06fa446917dd234bdf956b03d69b36c3a4d21cc2
soumadeb7 <soumadebkarmakar@gmail.com>
2026-03-19 16:56:06 +0530

    change app.py

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-from keras.models import load_model, Sequential
-from keras.layers import LSTM, Dense
+from tensorflow.keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
 
 st.title("📈 Stock Price Prediction (LSTM)")
 
-# Upload CSV
+# Load pre-trained model
+model = load_model("saved_model.h5")
+
 uploaded_file = st.file_uploader("Upload Stock CSV File", type=["csv"])
 
 if uploaded_file is not None:
@@ -32,32 +33,9 @@ if uploaded_file is not None:
     valid_data = new_dataset[int(len(new_dataset)*0.7):]
 
     scaler = MinMaxScaler(feature_range=(0,1))
-    scaled_train = scaler.fit_transform(train_data)
+    scaled_data = scaler.fit_transform(new_dataset)
 
-    x_train, y_train = [], []
-
-    for i in range(60, len(scaled_train)):
-        x_train.append(scaled_train[i-60:i, 0])
-        y_train.append(scaled_train[i, 0])
-
-    x_train, y_train = np.array(x_train), np.array(y_train)
-    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-
-    # Train Model
-    st.subheader("Training Model...")
-
-    model = Sequential()
-    model.add(LSTM(units=150, return_sequences=True, input_shape=(x_train.shape[1],1)))
-    model.add(LSTM(units=100))
-    model.add(Dense(1))
-
-    model.compile(loss='mean_squared_error', optimizer='adam')
-    model.fit(x_train, y_train, epochs=5, batch_size=2, verbose=0)
-
-    # Prepare test data
-    inputs = new_dataset[len(new_dataset) - len(valid_data) - 60:].values
-    inputs = inputs.reshape(-1,1)
-    inputs = scaler.transform(inputs)
+    inputs = scaled_data[len(scaled_data) - len(valid_data) - 60:]
 
     X_test = []
     for i in range(60, inputs.shape[0]):
@@ -82,14 +60,12 @@ if uploaded_file is not None:
     st.pyplot(fig)
 
     # Next Day Prediction
-    st.subheader(" Next Day Prediction")
+    st.subheader("🔮 Next Day Prediction")
 
     last_60 = new_dataset[-60:].values
     last_60 = scaler.transform(last_60)
 
-    X_pred = []
-    X_pred.append(last_60)
-    X_pred = np.array(X_pred)
+    X_pred = np.array([last_60])
     X_pred = np.reshape(X_pred, (X_pred.shape[0], X_pred.shape[1], 1))
 
     next_day = model.predict(X_pred)
